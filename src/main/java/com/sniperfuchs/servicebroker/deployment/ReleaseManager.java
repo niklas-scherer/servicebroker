da231d5e0fdb668dcc68a5881b65b321dc17f5b0
Niklas <scherer-niklas@web.de>
2021-04-24 00:44:57 +0200

    Added ReleaseManager

---
+++ src/main/java/com/sniperfuchs/servicebroker/deployment/ReleaseManager.java
@@ -0,0 +1,8 @@
+package com.sniperfuchs.servicebroker.deployment;
+
+public interface ReleaseManager {
+    public void install();
+    public void uninstall();
+    public void delete();
+    public void upgrade();
+}
